6bb5009280cd0b3c10543a24db06ddf2dde2cd77
Yigitalp Ertem <yalpertem@gmail.com>
2020-07-23 15:33:17 +0300

    Organized imports for test methods.

--- src/test/java/com/labforward/api/hello/controller/HelloControllerDeleteTest.java
+++ src/test/java/com/labforward/api/hello/controller/HelloControllerDeleteTest.java
@@ -1,33 +1,17 @@
 package com.labforward.api.hello.controller;
 
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.labforward.api.common.MVCIntegrationTest;
-import com.labforward.api.core.creation.EntityCreatedResponse;
-import com.labforward.api.core.validation.EntityValidator;
 import com.labforward.api.hello.domain.Greeting;
-import com.labforward.api.hello.service.HelloWorldService;
 
 public class HelloControllerDeleteTest extends HelloControllerTest {
 	
